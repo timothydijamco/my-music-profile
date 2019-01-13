cc3ddf3b58d669ef95090cc020b302d33020ba91
Timothy Dijamco <contact@timothydijamco.com>
2019-01-13 14:01:37 -0500

    Combine server and client modules

--- index.js
+++ index.js
@@ -21,20 +21,23 @@ mongoose.Promise = global.Promise;
 
 // == Middleware == //
 
+app.use('/', express.static('./client-web/build'));
+
 // Allow cross-origin requests
-app.use((req, res, next) => {
+app.use('/api', (req, res, next) => {
    res.header("Access-Control-Allow-Origin", "*");
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
    next();
 });
 
 // Parse incoming data
-app.use(bodyParser.json());
+app.use('/api', bodyParser.json());
 
+// Route API
 app.use('/api', routes);
 
 // Print errors
-app.use((err, req, res, next) => {
+app.use('/api', (err, req, res, next) => {
    console.log(err);
    next();
 });
