4809f475a8381294088629b50e9fd5218860b230
Timothy Dijamco <contact@timothydijamco.com>
2019-01-13 16:52:15 -0500

    Hook profile viewing page into backend (so that it pulls the profile data)

--- client-web/src/Profile.js
+++ client-web/src/Profile.js
@@ -2,9 +2,41 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class Profile extends Component {
+
+   constructor(props) {
+      super(props);
+      this.state = {
+         favoriteArtist: "",
+         favoriteAlbumEp: "",
+         favoriteTrack: "",
+         firstShow: "",
+         mostRecentShow: "",
+         favoriteShow: ""
+      };
+   }
+
+   componentDidMount(){
+      this.getProfile();
+   }
+
+   getProfile() {
+      axios.get(`/api/profile/${this.props.match.params.code}`, this.state)
+         .then(res => {
+            this.setState({
+               favoriteArtist: res.data.favoriteArtist,
+               favoriteAlbumEp: res.data.favoriteAlbumEp,
+               favoriteTrack: res.data.favoriteTrack,
+               firstShow: res.data.firstShow,
+               mostRecentShow: res.data.mostRecentShow,
+               favoriteShow: res.data.favoriteShow
+            });
+         })
+         .catch(err => console.log(err));
+   }
+
    render() {
       return (
-         <p>{this.props.match.params.code}</p>
+         <p>{this.state.favoriteArtist}</p>
       );
    }
 }
